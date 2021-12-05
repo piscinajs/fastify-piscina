0e85be1e5e61408e7553cf70beb6fcc66413b69e
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-12-05 13:28:48 +0100

    types: update types

--- plugin.d.ts
+++ plugin.d.ts
@@ -1,32 +1,19 @@
-import Piscina from "piscina";
-import { Plugin } from "fastify";
-import { ServerResponse, IncomingMessage, Server } from "http";
-import { Http2Server, Http2ServerRequest, Http2ServerResponse } from "http2";
-
-type HttpServer = Server | Http2Server;
-type HttpRequest = IncomingMessage | Http2ServerRequest;
-type HttpResponse = ServerResponse | Http2ServerResponse;
+import Piscina from 'piscina';
+import { FastifyPluginCallback } from 'fastify';
 
 type PiscinaOptions = typeof Piscina extends {
-  new (options?: infer T): Piscina;
-}
+    new (options?: infer T): Piscina;
+  }
   ? T
   : never;
 
-interface PiscinaPlugin
-  extends Plugin<HttpServer, HttpRequest, HttpResponse, PiscinaOptions> {}
-
-declare const piscinaPlugin: PiscinaPlugin;
-export = piscinaPlugin;
-
 // Most importantly, use declaration merging to add the custom property to the Fastify type system
 declare module "fastify" {
-  interface FastifyInstance<
-    HttpServer = Server,
-    HttpRequest = IncomingMessage,
-    HttpResponse = ServerResponse
-  > {
+  interface FastifyInstance {
     piscina: Piscina;
-    runTask: Piscina["runTask"];
+    runTask: Piscina['run'];
   }
 }
+
+declare const fastifyPiscina: FastifyPluginCallback<PiscinaOptions>;
+export default fastifyPiscina;
